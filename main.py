61761792073389b62c58eaad25f1673f817e69e7
dimaMukan <ds.mukan@gmail.com>
2026-03-04 20:51:04 +0000

    Done: - Add a simple frontend for better visibility
    Next steps:
    - Work with Alembic
    - Understand how Wallet Transaction get/del/update work
    
    Future ideas:
    - Add a telegram bot for messages
    - Deploy with Docker
    - Add registrations
    -

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from pathlib import Path
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from routers.wallet_router import router as wallet_router
 from routers.transaction_router import router as transaction_router
 from db.database import engine
@@ -7,6 +9,12 @@ from models import Wallet, Transaction
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+
+
+@app.get("/", include_in_schema=False)
+def frontend():
+    return FileResponse(Path(__file__).parent / "frontend" / "index.html")
+
 
 app.include_router(wallet_router)
 app.include_router(transaction_router)
